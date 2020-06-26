5c7c598950bba576f22063e1698df2ef2d2f3785
Julian <julian@jgroen.com>
2020-06-26 08:47:57 -0600

    updated to async / await syntax

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-fetch('http://api.tvmaze.com/shows/1121')
-    .then((response) => {
-        if (!response.ok) {
-            throw new Error(`${response.status} - ${response.statusText}`);
-        }
-        return response.json();
-    })
+async function fetchShow(url) {
+    let response = await fetch(url);
+
+    if (!response.ok) {
+        throw new Error(`${response.status} - ${response.statusText}`);
+    } else {
+        return await response.json();
+    }
+}
+
+let showUrl = 'http://api.tvmaze.com/shows/1121';
+
+fetchShow(showUrl)
     .then((data) => {
-        document.body.innerHTML = `<h1>${data.name}</h1><img src="${data.image.medium}" alt=""/><h1>Episodes:</h1>`;
-        return fetch(`${data._links.self.href}/episodes`);
-    })
-    .then((response) => {
-        if (!response.ok) {
-            throw new Error(`${response.status} - ${response.statusText}`);
-        }
-        return response.json();
+        document.body.innerHTML += `<h1>${data.name}</h1><img src="${data.image.medium}" alt=""/><h1>Episodes:</h1>`;
+        return fetchShow(`${data._links.self.href}/episodes`);
     })
     .then((data) => {
         data.map((episode) => {
